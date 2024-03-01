86fa614b9ebce20d52c798d4d8905c69b3b2af6a
Divyasunilraj <A00316073@student.tus.ie>
2024-03-01 12:23:27 +0000

    first commit

---
+++ Desktop/Practise1/src/com/example/One.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class One {
+	String message = "foo";
+	  String message2 = "toto";
+
+	  public String foo() {
+	    return message;
+	  }
+
+	  public String toto() {
+	    return message2;
+	  }
+
+	  public void uncoveredMethod() {
+	    System.out.println(foo());
+	  }
+
+}
